fc0d53a8795f7393e74ab8344c30f5531c058142
Rasha <rasha.k.alsaleh@gmail.com>
2024-01-05 01:04:04 +0300

    changed header

--- header.php
+++ header.php
@@ -9,14 +9,19 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
     nav {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 1000;
+      position: fixed;
+      top: 0;
+      width: 100%;
+      z-index: 1000;
     }
   </style>
 </head>
 <body>
+
+<?php
+// Get the current page name from the PHP_SELF variable
+$current_page = basename($_SERVER['PHP_SELF']);
+?>
 
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
@@ -24,17 +29,17 @@
       <a class="navbar-brand" href="main_page.php">My website</a>
     </div>
     <ul class="nav navbar-nav">
-        <li class="active"><a href="home.php">Home</a></li>
-        <li><a href="order.php">View Data</a></li>
-        <li><a href="search.php">Search Data</a></li>
-        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Sitting<span class="caret"></span></a>
+      <li <?php if ($current_page == 'home.php') echo 'class="active"'; ?>><a href="home.php">Home</a></li>
+      <li <?php if ($current_page == 'order.php') echo 'class="active"'; ?>><a href="order.php">View Data</a></li>
+      <li <?php if ($current_page == 'search.php') echo 'class="active"'; ?>><a href="search.php">Search Data</a></li>
+      <li class="dropdown <?php if ($current_page == 'edit.php' || $current_page == 'logout.php' || $current_page == 'delete.php') echo 'active'; ?>">
+        <a class="dropdown-toggle" data-toggle="dropdown">Sitting<span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="edit.php">Edit Profile</a></li>
           <li><a href="logout.php">Log Out</a></li>
           <li><a href="delete.php">Delete an account</a></li>
         </ul>
       </li>
-      
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
@@ -42,8 +47,6 @@
     </ul>
   </div>
 </nav>
-  
-
 
 </body>
 </html>
